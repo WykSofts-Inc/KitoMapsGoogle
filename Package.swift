f356e399909494f9ed71539767345c772366156c
Wycliff <wycliff@wyksoftsinc.com>
2026-09-23 16:03:00 +0300

    chore: scaffold KitoMapsGoogle

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.9
+//
+//  Package.swift
+//  KitoMapsGoogle
+//
+//  Created by Wycliff on 9/23/26.
+//  Copyright © 2026 wyksoftsinc.com. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "KitoMapsGoogle",
+    platforms: [.iOS(.v17)],
+    products: [.library(name: "KitoMapsGoogle", targets: ["KitoMapsGoogle"])],
+    dependencies: [
+        .package(url: "https://github.com/WykSofts-Inc/KitoMaps.git", from: "0.1.0"),
+        .package(url: "https://github.com/googlemaps/ios-maps-sdk", from: "11.1.0"),
+    ],
+    targets: [
+        .target(name: "KitoMapsGoogle", dependencies: [
+            .product(name: "KitoMaps", package: "KitoMaps"),
+            .product(name: "GoogleMaps", package: "ios-maps-sdk"),
+        ]),
+        .testTarget(name: "KitoMapsGoogleTests", dependencies: ["KitoMapsGoogle"]),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# KitoMapsGoogle
+
+KitoMaps pins, clusters and cards on the Google Maps SDK. Part of the [Kito](https://github.com/WykSofts-Inc/KitoDevKit) ecosystem.
